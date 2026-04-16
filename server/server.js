3def9eae14cb9f8fe7f2e47befb1239a7ca6c3b8
hwykiynt <clairewouldliketosignthings@gmail.com>
2026-04-16 15:57:51 -0400

    api call

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
 //backend should go
+const { getJson } = require("serpapi");
+
+getJson({
+  engine: "yelp",
+  find_desc: "food",
+  find_loc: "New Brunswick, New Jersey, United States",
+  api_key: "1caaf9f9790e745ee0ae0e72a6fd68b1db3b40c5147d1af18c64682548ab0b40"
+}, (json) => {
+  console.log(json["organic_results"]);
+});
